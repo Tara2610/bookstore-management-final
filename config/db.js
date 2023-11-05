8dd8c2fa684cfc2f0317bb407c27d9f04130bacc
tara <kalvatara554@gmail.com>
2023-11-05 20:06:27 +0530

    first commit

---
+++ config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+mongoose.connect('mongodb://localhost:27017/Book-Store');
+
+const db = mongoose.connection;
+
+db.on('error',(error)=>{
+    console.log('error to connecting with database')
+})
+
+db.once('open',function(){
+    console.log('Successfully connected to database');
+})
+
